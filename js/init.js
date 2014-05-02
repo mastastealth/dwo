1c9a6d4b2b5bd929a792c477348bd59a07acb289
Brian Franco <mastastealth+git@gmail.com>
2014-05-02 12:52:23 -0400

    Hide Quit from Game

--- js/init.js
+++ js/init.js
@@ -335,7 +335,6 @@ document.addEventListener('DOMContentLoaded', function(){
 	});
 
 	// Quit Button 
-	/*
 	document.querySelector('.game .menu').addEventListener('click', function() {
 		if ( buoy.hasClass(document.querySelector('.game'), 'tut') ) {
 			wipeGame();
@@ -348,7 +347,6 @@ document.addEventListener('DOMContentLoaded', function(){
 			m.innerHTML = '';
 		}
 	});
-	*/
 
 	validDeck = function(deck) {
 		// If there are 25 supplies
